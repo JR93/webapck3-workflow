2a9d1879df35144eb3cd1a9f5a2eba86277b41d5
guojinrong <guojinrong@yy.com>
2018-03-07 17:13:58 +0800

    init

--- src/rank.js
+++ src/rank.js
@@ -1,14 +1,8 @@
-// import 'css/rank.scss';
 import App from 'component/rank/app.vue';
-import Filters from 'common/filter';
 
 if (__DEV__) { require('html/pages/rank.hbs'); } // eslint-disable-line global-require
 
 document.addEventListener('touchstart', () => {});
-
-Object.keys(Filters).forEach((key) => {
-  Vue.filter(key, Filters[key]);
-});
 
 new Vue({ // eslint-disable-line
   el: '#app',
@@ -16,4 +10,3 @@ new Vue({ // eslint-disable-line
   components: { App },
 });
 
-console.log(Vue);
